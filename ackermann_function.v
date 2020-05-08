d68e1e306bdc075b2cd809038ea4ad1b8b9db4b7
mar2tir <martin2reimann@gmail.com>
2020-05-08 18:22:07 +0200

    update ackermann

--- ackermann_function.v
+++ ackermann_function.v
@@ -1,32 +1,20 @@
 module main
 
-fn ackermann(n, m u32) u32 {
-	if n == 0 {
-		return m + 1
-	} else if m == 0 {
-		return ackermann(n - 1, 1)
+fn ackermann(m, n u32) u32 {
+	if m == 0 {
+		return n + 1
+	} else if n == 0 {
+		return ackermann(m - 1, 1)
 	} else {
-		return ackermann(n - 1, ackermann(n, m - 1))
+		return ackermann(m - 1, ackermann(m, n - 1))
 	}
 }
 
 fn main() {
-	print('ackermann(0, 0)')
-	println(ackermann(0, 0))
-	print('ackermann(1, 1)')
-	println(ackermann(1, 1))
-	print('ackermann(2, 2)')
-	println(ackermann(2, 2))
-	print('ackermann(3, 3)')
-	println(ackermann(3, 3))
-	print('ackermann(4, 0)')
-	println(ackermann(4, 0))
-	print('ackermann(3, 4)')
-	println(ackermann(3, 4))
-	print('ackermann(3, 11)')
-	println(ackermann(3, 11))
-	print('ackermann(3, 13)')
-	println(ackermann(3, 13))
-	print('ackermann(4, 1)')
-	println(ackermann(4, 1))
+        for m := 0; m <= 4; m++ {
+                for n := 0; n < 6 - m; n++ {
+                        println('Ackermann($m, $n) = ${ackermann(m, n)}')
+                }
+        }
 }
+ 
